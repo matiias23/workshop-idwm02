ff2cbd257e36a863585b807286949514c9adea9a
Matiias23 <n_matias@hotmail.es>
2023-11-05 19:22:15 -0300

    [Frontend] feat: components users and routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './_components/home/home.component';
 import { LoginComponent } from './_components/login/login.component';
+import { UserListComponent } from './_components/user/user-list/user-list.component';
+import { UserCreateComponent } from './_components/user/user-create/user-create.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {path: 'users/list', component:UserListComponent},
+  {path: 'users/create', component:UserCreateComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './_components/home/home.component';
 import { NavComponent } from './_components/nav/nav.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { LoginComponent } from './_components/login/login.component';
+import { UserListComponent } from './_components/user/user-list/user-list.component';
+import { RouterModule } from '@angular/router';
+import { UserCreateComponent } from './_components/user/user-create/user-create.component';
 
 
 @NgModule({
@@ -15,13 +18,16 @@ import { LoginComponent } from './_components/login/login.component';
     HomeComponent,
     NavComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    UserListComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
