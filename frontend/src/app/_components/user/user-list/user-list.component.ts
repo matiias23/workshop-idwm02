adb10d19f532d0226cbb05f1eafb4ed33987b35c
Matiias23 <n_matias@hotmail.es>
2023-11-19 21:16:35 -0300

    [Frontend] feat: GetUsers and CORS

--- frontend/src/app/_components/user/user-list/user-list.component.ts
+++ frontend/src/app/_components/user/user-list/user-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/_services/user.service';
+import { User } from 'src/app/_models/user';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[];
+
+  constructor(
+    private userService: UserService,
+    private router: Router) {
+      this.users=[];
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 
 }
--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -13,7 +13,12 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.apiUrl}/users`);
+    return this.http.get<User[]>(`${this.apiUrl}/users`).pipe(
+      map(users => {
+        this.users = users;
+        return users;
+      })
+    )
   }
 
   getUserById(userId: number): Observable<User> {
