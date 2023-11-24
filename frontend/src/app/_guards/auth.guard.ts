c0c03ffa62e979fa63475b237a50a5c6c4ed832e
Matiias23 <n_matias@hotmail.es>
2023-11-24 20:04:30 -0300

    [Frontend] feat: Protected Routes with Guard

---
+++ frontend/src/app/_guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {map, Observable} from 'rxjs';
+import { AccountService } from '../_services/account.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private accountService: AccountService) {
+  }
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentAccount$.pipe(
+      map(account => {
+        return !!account;
+      })
+    )
+  }
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,28 @@ import { LoginComponent } from './_components/login/login.component';
 import { UserListComponent } from './_components/user/user-list/user-list.component';
 import { UserCreateComponent } from './_components/user/user-create/user-create.component';
 import { UserUpdateComponent } from './_components/user/user-update/user-update.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'home', component:HomeComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'users/list', component:UserListComponent},
-  {path: 'users/create', component:UserCreateComponent},
-  {path: 'users/:id', component:UserUpdateComponent}
+  {
+    path: '',
+    pathMatch: 'full',  // Asegura que la ruta esté vacía
+    redirectTo: 'login',
+  },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'home', component:HomeComponent},
+      {path: 'users/list', component:UserListComponent},
+      {path: 'users/create', component:UserCreateComponent},
+      {path: 'users/:id', component:UserUpdateComponent},
+    ]
+  },
+  { path: '**', redirectTo: '/login' },
+
 ];
 
 @NgModule({
